0c60b8a144f077a175c009ed4dacda92f21739c2
Grace Garcia <gracegarcia0803@gmail.com>
2020-11-25 21:25:52 +0800

    Added header, footer, product, and rating component, added homepage, and implements react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to GadgetShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <Route path='/' component={HomePage} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
