9a947396fc1723e3cd5c98925f50cc5bdc05358c
Grace Garcia <gracegarcia0803@gmail.com>
2020-11-25 20:53:16 +0800

    Initialize setup

---
+++ client/src/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Razer Hammerhead True Wireless Earbuds',
+    image: '/images/tws.jpg',
+    description:
+      'True Wireless Freedom. Listen without limitation and take your audio immersion to new heights with the Razer Hammerhead True Wireless Earbuds—packed with performance for seamless, high-quality sound you can enjoy anywhere',
+    brand: 'Razer',
+    category: 'Electronics',
+    price: 3795.0,
+    countInStock: 22,
+    rating: 4.5,
+    numReviews: 28,
+  },
+  {
+    _id: '2',
+    name: 'Apple Macbook Air 13" 256GB 2020 Edition',
+    image: '/images/air.jpeg',
+    description:
+      'The incredibly thin and light MacBook Air is now more powerful than ever. It features a brilliant Retina display, new Magic Keyboard, Touch ID, processors with up to twice the performance,1 faster graphics, and double the storage capacity.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 57990.0,
+    countInStock: 18,
+    rating: 4.5,
+    numReviews: 7,
+  },
+  {
+    _id: '3',
+    name: 'Asus ROG Strix Scar G532LW-AZ071TS',
+    image: '/images/scar.jpg',
+    description:
+      'Exceptional speed, power, and performance elevate the ROG Strix SCAR 15/17 to the top tier of Windows 10 gaming. The latest 10th Gen Intel® Core™ i7 processors and powerful GeForce® RTX™ graphics saturate the cutting-edge 300Hz/3ms gaming panel that takes you to the next frontier of pro esports speed.',
+    brand: 'Asus',
+    category: 'Electronics',
+    price: 139995.0,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 10,
+  },
+  {
+    _id: '4',
+    name: 'Xbox One S 1TB Two Controller Bundle',
+    image: '/images/xbox.jpg',
+    description:
+      "Xbox One is the only console designed to play the best games of the past, present, and future. Play the best Xbox One exclusives like Forza Motorsport 7, Cuphead, Halo, Sea of Thieves, and PlayerUnknown's Battlegrounds, and play all the biggest blockbusters like Call of Duty WWII, Star WarsTM BattlefrontTM II, FIFA 18, and Middle-earth: Shadow of War.",
+    brand: 'Microsoft',
+    category: 'Electronics',
+    price: 14995.0,
+    countInStock: 12,
+    rating: 3,
+    numReviews: 8,
+  },
+  {
+    _id: '5',
+    name: 'PS4 Slim 1TB Mega Pack 2 Bundle',
+    image: '/images/ps4.png',
+    description:
+      "Endless Entertainment Something new and amazing is always in reach. Find what you're looking for and get it at the touch of a button via PlayStation entertainment options like PlayStation Vue and more.",
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 17990.0,
+    countInStock: 15,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Nintendo Switch v2 with Neon Joy-Con',
+    image: '/images/switch.jpg',
+    description:
+      'Get the gaming system that lets you play the games you want, wherever you are, however you like.',
+    brand: 'Nintendo',
+    category: 'Electronics',
+    price: 16995,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 28,
+  },
+  {
+    _id: '7',
+    name: 'Asus ROG Phone 3 512GB 12GB ZS661KS',
+    image: '/images/phone.jpg',
+    description:
+      'ROG Phone 3 is the next evolution of the gaming smartphone, delivering amazing experiences for all types of gamers.',
+    brand: 'Asus',
+    category: 'Electronics',
+    price: 49995.0,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 3,
+  },
+];
+
+module.exports = products;
