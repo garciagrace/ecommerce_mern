efb34610ef5f641f440326ef85e4792e64c114e5
Grace Garcia <gracegarcia0803@gmail.com>
2020-12-25 20:51:35 +0800

    Added update profile functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -76,4 +76,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+// Desc:   Update user profile
+// Route:  PUT /api/users/profile
+// Access: Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status;
+    throw new Error();
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
