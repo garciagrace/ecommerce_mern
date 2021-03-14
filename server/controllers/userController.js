99a101bfbc7e5692bbc78984eedb07babd2c7559
Grace Garcia <garciagrace0028@gmail.com>
2021-03-14 14:34:34 +0800

    Added admin middleware and get users endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -104,4 +104,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile };
+// Desc:   Get all users
+// Route:  GET /api/users
+// Access: Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
