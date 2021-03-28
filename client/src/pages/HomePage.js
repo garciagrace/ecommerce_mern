a58c4ae456eb6ca0025e2c28722a197c8cf56c43
Grace Garcia <garciagrace0028@gmail.com>
2021-03-28 21:07:03 +0800

    Added product carousel component

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 import { listProducts } from '../actions/productActions';
 
 const HomePage = ({ match }) => {
@@ -23,6 +25,13 @@ const HomePage = ({ match }) => {
 
   return (
     <>
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
