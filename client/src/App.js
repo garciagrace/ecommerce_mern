c4a69d64d6a6c797a9887ae3ad8e5a28dc51a259
Grace Garcia <garciagrace0028@gmail.com>
2021-03-28 12:16:25 +0800

    Added pagination functionality

--- client/src/App.js
+++ client/src/App.js
@@ -40,6 +40,12 @@ const App = () => {
           <Route path='/admin/product/:id/edit' component={ProductEditPage} />
           <Route path='/admin/orderlist' component={OrderListPage} />
           <Route path='/search/:keyword' component={HomePage} />
+          <Route path='/page/:pageNumber' component={HomePage} exact />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomePage}
+            exact
+          />
           <Route path='/' component={HomePage} exact />
         </Container>
       </main>
