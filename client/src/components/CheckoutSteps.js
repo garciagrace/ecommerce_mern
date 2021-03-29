c2a0dc21601ce2b28414a0de2e388f6e863bf73d
Grace Garcia <garciagrace0028@gmail.com>
2021-03-29 17:10:59 +0800

    Revert "refractored check out steps"
    This reverts commit 9c8b924147942721274c1df03165b0e1b191b694.

--- client/src/components/CheckoutSteps.js
+++ client/src/components/CheckoutSteps.js
@@ -6,27 +6,42 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
     <Nav className='justify-content-center mb-4'>
       <Nav.Item>
-        <LinkContainer to='/login'>
+        {step1 ? (
+          <LinkContainer to='/login'>
+            <Nav.Link>Sign In</Nav.Link>
+          </LinkContainer>
+        ) : (
           <Nav.Link disabled>Sign In</Nav.Link>
-        </LinkContainer>
+        )}
       </Nav.Item>
 
       <Nav.Item>
-        <LinkContainer to='/shipping'>
-          <Nav.Link disabled={!step2}>Shipping</Nav.Link>
-        </LinkContainer>
+        {step2 ? (
+          <LinkContainer to='/shipping'>
+            <Nav.Link>Shipping</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
       </Nav.Item>
 
       <Nav.Item>
-        <LinkContainer to='/payment'>
-          <Nav.Link disabled={!step3}>Payment</Nav.Link>
-        </LinkContainer>
+        {step3 ? (
+          <LinkContainer to='/payment'>
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
       </Nav.Item>
-
       <Nav.Item>
-        <LinkContainer to='/placeorder'>
-          <Nav.Link disabled={!step4}>Place Order</Nav.Link>
-        </LinkContainer>
+        {step4 ? (
+          <LinkContainer to='/placeorder'>
+            <Nav.Link>Place Order</Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>Place Order</Nav.Link>
+        )}
       </Nav.Item>
     </Nav>
   );
